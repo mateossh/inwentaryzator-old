8209eac32378ae4b36649bd89b3d069b93e0b453
Mateusz Ż <zochowski.mateusz@outlook.com>
2021-05-31 11:18:47 +0200

    Style toasts using tailwind

--- frontend/src/components/ToastWrapper.js
+++ frontend/src/components/ToastWrapper.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Toast from 'react-bootstrap/Toast';
 import { hideToast } from '../actions';
-
-import '../stylesheets/ToastWrapper.css';
 
 export const ToastWrapper = () => {
   const dispatch = useDispatch();
-
   const toasts = useSelector(state => state.toast.toasts);
 
-  /* NOTE: I took aria things straight from bootstrap docs:
-            https://getbootstrap.com/docs/4.3/components/toasts/#placement */
+  // NOTE: Aria things are from react-bootstrap
+  // https://getbootstrap.com/docs/4.3/components/toasts/#placement
 
   return (
     <div
-      className="toast-wrapper"
+      className="m-0 fixed top-4 right-4 z-50"
       aria-live="polite"
       aria-atomic="true"
     >
       {toasts.length > 0 && toasts.map(toast => (
-        <Toast
-          className="p-1"
-          show={toast.isVisible}
-          onClose={() => dispatch(hideToast(toast.id))}
+        <div
+          className={`mb-2 p-2 max-w-xl bg-white border rounded ${toast.isVisible ? '' : 'hidden'}`}
           key={toast.id}
         >
-        <Toast.Header>
-          <strong className="mr-auto">{toast.title}</strong>
-        </Toast.Header>
-        <Toast.Body>{toast.message}</Toast.Body>
-      </Toast>
+          <div className="px-1 m-0 text-gray-800 font-semibold flex flex-row justify-between">
+            <div className="text-sm leading-6">{toast.title}</div>
+            <button className="-mt-1 text-lg" onClick={() => dispatch(hideToast(toast.id))}>
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <hr className="my-1 w-full" />
+          <div className="mx-1 mt-2 mb-1 text-sm">
+            {toast.message}
+          </div>
+        </div>
       ))}
     </div>
   );
