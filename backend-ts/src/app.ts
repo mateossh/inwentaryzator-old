74038622946e9da4b1e187b536c8e766904a4ea7
Mateusz Ż <zochowski.mateusz@outlook.com>
2022-10-01 21:48:31 +0200

    backend: add cors

--- backend-ts/src/app.ts
+++ backend-ts/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import * as bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { router as productsRouter } from './routes/products';
 import { router as stockRouter } from './routes/stock';
@@ -7,6 +8,7 @@ import { router as stockRouter } from './routes/stock';
 const app: Express = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/v2/product', productsRouter);
 app.use('/api/v2/stock', stockRouter);
